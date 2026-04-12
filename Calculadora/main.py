9f575ff8daa3389673080b92b663fbc967ee93e5
Adeniltonq <quarrsmaadenilton@gmail.com>
2026-04-12 16:45:21 -0300

    Modificando  arquivo main, criando arquivo button com classe ButtonGrid.

--- Calculadora/main.py
+++ Calculadora/main.py
@@ -1,42 +1,39 @@
-from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel, QLineEdit
-
 import sys
 
-from main_window import MainWindow
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QApplication 
+
+from button import ButtonsGrid
 from display import Display
 from info import Info
+from main_window import MainWindow
 
+from styles import setupTheme
 from variables import WINDOW_ICON_DIR
-
+ 
 if __name__ == '__main__':
-    app =QApplication(sys.argv)
-
-# ->QApli ->QMainwin ->Qwidget->QVBoxLAy ->
-
+    
+    app = QApplication(sys.argv)
+    setupTheme(app)
     window = MainWindow()
-
-    #label1 = QLabel("Olá Mundo")
-    #label1.setStyleSheet("font-size: 50px")
-    #window.addWidgetToVLayout(label1)
-
+ 
+ 
     icon = QIcon(str(WINDOW_ICON_DIR))
-
-    info = Info('2.0 ^ 10.0 = 1024')
-    window.addWidgetToVLayout(info)
-
-
-    display = Display()
-    window.addWidgetToVLayout(display )
-
+    window.setWindowIcon(icon)
     app.setWindowIcon(icon)
-
-
-
-
-
+ 
+  
+    info = Info('Sua conta')
+    window.addWidgetToVLayout(info)
+ 
+   
+    display = Display()
+    window.addWidgetToVLayout(display)
+ 
+    buttonsGrid = ButtonsGrid()
+    window.vLayout.addLayout(buttonsGrid)
+ 
+   
+  
     window.show()
-
-
     app.exec()
-
