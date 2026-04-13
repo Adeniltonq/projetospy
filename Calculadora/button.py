b97d4eb95fb70f45bfb3688ddbfa8047b6d5d6e1
Adeniltonq <quarrsmaadenilton@gmail.com>
2026-04-13 15:22:17 -0300

    buttons da calculadora

--- Calculadora/button.py
+++ Calculadora/button.py
@@ -1,7 +1,9 @@
 from PySide6.QtWidgets import QPushButton, QGridLayout
+from PySide6.QtCore import Slot
 
 from variables import MEDIUM_FONT_SIZE
 from utils import isNotNumOrDotAndIsEmpty
+from display import Display
 
 
 class  Button(QPushButton):
@@ -14,8 +16,9 @@ class  Button(QPushButton):
         self.font().setPixelSize(MEDIUM_FONT_SIZE)
         self.setMinimumSize(75,75)
 
+
 class ButtonsGrid(QGridLayout):
-    def __init__(self,*args ,**kwargs):
+    def __init__(self,display:Display,*args ,**kwargs):
         super().__init__(*args ,**kwargs)
       
 
@@ -26,6 +29,8 @@ class ButtonsGrid(QGridLayout):
             ['1','2','3','='],
             ['','0','.','=']
         ]
+
+        self.display = display
         self._makeGrid()
     def _makeGrid(self):
         for i, row in enumerate(self._grid_mask):
@@ -36,9 +41,21 @@ class ButtonsGrid(QGridLayout):
                       button.setStyleSheet("background-color: blue; color: white;")
 
                 self.addWidget(button, i, j)
+                buttonSlot = self._makeButtonDisplaySlot(self._insertButtonTextToDisplay,button)
+                button.clicked.connect(buttonSlot)
                 
+    def _makeButtonDisplaySlot(self, func, *args, **kwargs):
 
+        @Slot()
+        def realSlot(checked):
+              func(checked,*args, **kwargs)
+        return realSlot
 
+    def _insertButtonTextToDisplay(self,checked,button):
+            
+            self.display.insert(button.text())
+            print(checked)
+            
         
     
      
